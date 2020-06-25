6cee3412c55c532a0d9295d98d3e9dbbe2642e64
EthanLin <ethan510010@gmail.com>
2020-06-25 23:16:49 +0800

    add test error case

--- __tests__/heroes.test.js
+++ __tests__/heroes.test.js
@@ -9,41 +9,61 @@ set jest timeout to 9000ms instead of default value 5000ms
 */
 it('test no auth heroes api', async (done) => {
   const response = await request.get('/heroes');
-  const heroObjKeys = Object.keys(response.body.heroes[0]);
-  expect(heroObjKeys).toContain('id');
-  expect(heroObjKeys).toContain('name');
-  expect(heroObjKeys).toContain('image');
-  expect(heroObjKeys).not.toContain('profile');
+  if (response.status !== 200) {
+    const errorResKeys = Object.keys(response.body);
+    expect(errorResKeys).toContain('message');
+  } else {
+    const heroObjKeys = Object.keys(response.body.heroes[0]);
+    expect(heroObjKeys).toContain('id');
+    expect(heroObjKeys).toContain('name');
+    expect(heroObjKeys).toContain('image');
+    expect(heroObjKeys).not.toContain('profile');
+  }
   done();
 }, 9000);
 
 it('test no auth single hero api', async (done) => {
   const heroId = '1';
   const response = await request.get(`/heroes/${heroId}`);
-  expect(response.body.id).toBe(heroId);
-  const heroObjKeys = Object.keys(response.body);
-  expect(heroObjKeys).not.toContain('profile');
+  if (response.status !== 200) {
+    const errorResKeys = Object.keys(response.body);
+    expect(errorResKeys).toContain('message');
+  } else {
+    expect(response.body.id).toBe(heroId);
+    const heroObjKeys = Object.keys(response.body);
+    expect(heroObjKeys).not.toContain('profile');
+  }
   done();
 }, 9000);
 
 it('test auth heroes api', async (done) => {
   const response = await request.get('/heroes').set('Name', 'hahow').set('Password', 'rocks');
-  const heroObjKeys = Object.keys(response.body.heroes[0]);
-  expect(heroObjKeys).toContain('id');
-  expect(heroObjKeys).toContain('name');
-  expect(heroObjKeys).toContain('image');
-  expect(heroObjKeys).toContain('profile');
+  if (response.status !== 200) {
+    const errorResKeys = Object.keys(response.body);
+    expect(errorResKeys).toContain('message');
+  } else {
+    const heroObjKeys = Object.keys(response.body.heroes[0]);
+    expect(heroObjKeys).toContain('id');
+    expect(heroObjKeys).toContain('name');
+    expect(heroObjKeys).toContain('image');
+    expect(heroObjKeys).toContain('profile');
+  }
   done();
 }, 9000);
 
 it('test auth single hero api', async (done) => {
   const heroId = '1';
   const response = await request.get(`/heroes/${heroId}`).set('Name', 'hahow').set('Password', 'rocks');
-  expect(response.body.id).toBe(heroId);
-  const heroObjKeys = Object.keys(response.body);
-  expect(heroObjKeys).toContain('id');
-  expect(heroObjKeys).toContain('name');
-  expect(heroObjKeys).toContain('image');
-  expect(heroObjKeys).toContain('profile');
+  if (response.status !== 200) {
+    const errorResKeys = Object.keys(response.body);
+    expect(errorResKeys).toContain('message');
+  } else {
+    expect(response.body.id).toBe(heroId);
+    const heroObjKeys = Object.keys(response.body);
+    expect(heroObjKeys).toContain('id');
+    expect(heroObjKeys).toContain('name');
+    expect(heroObjKeys).toContain('image');
+    expect(heroObjKeys).toContain('profile');
+  }
   done();
 }, 9000);
