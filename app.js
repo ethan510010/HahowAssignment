9ff95cbe2001efb689aff258d788909cb8f05bf0
EthanLin <ethan510010@gmail.com>
2020-06-27 00:30:30 +0800

    add home page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const homeRouter = require('./routes/index');
 const heroesRouter = require('./routes/heroes');
 
 const app = express();
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', homeRouter);
 app.use('/heroes', heroesRouter);
 
 // catch 404 and forward to error handler
